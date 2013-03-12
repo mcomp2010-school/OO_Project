642cc69aa03adb7fd4f62e4b03759e7f915acae4
manny02 <manny02@manny02-LAPTOP>
2013-03-12 19:46:40 -0400

    Added Menu, Menu item, Iterators

---
+++ ProjectOO/src/org/Iterators/CompleteMenuIterator.java
@@ -0,0 +1,63 @@
+package org.Iterators;
+
+import java.util.ArrayList;
+
+import org.menu.Menu;
+import org.menu.MenuItem;
+
+
+// TODO: Auto-generated Javadoc
+/**
+ * The Class CompleteMenuIterator.
+ */
+public class CompleteMenuIterator implements MenuIterator {
+
+	/** The base. */
+	private Menu base;
+	
+	/** The position. */
+	private int position;
+
+	/**
+	 * Instantiates a new complete menu iterator.
+	 *
+	 * @param menuObj the menu obj
+	 */
+	public CompleteMenuIterator(Menu menuObj) {
+		// TODO Auto-generated constructor stub
+		this.base=menuObj;
+	}
+
+
+	/* (non-Javadoc)
+	 * @see org.Iterators.MenuIterator#hasNext()
+	 */
+	@Override
+	public boolean hasNext() {
+
+		if (position < base.size())
+			return true;
+		else
+			return false;
+	}
+
+	/* (non-Javadoc)
+	 * @see org.Iterators.MenuIterator#next()
+	 */
+	@Override
+	public Object next() {
+		Object aniObj = base.get(position);
+		position++;
+		return aniObj;
+	}
+
+	/* (non-Javadoc)
+	 * @see org.Iterators.MenuIterator#remove()
+	 */
+	@Override
+	public void remove() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ ProjectOO/src/org/Iterators/HeartHealthyMenuIterator.java
@@ -0,0 +1,87 @@
+package org.Iterators;
+
+import org.menu.Menu;
+import org.menu.MenuItem;
+
+/**
+ * The Class HeartHealthyMenuIterator.
+ */
+public class HeartHealthyMenuIterator implements MenuIterator {
+
+	/** The base. */
+	private Menu base;
+	
+	/** The position. */
+	private int position=0;
+
+	/**
+	 * Instantiates a new heart healthy menu iterator.
+	 *
+	 * @param menuObj the menu obj
+	 */
+	public HeartHealthyMenuIterator(Menu menuObj) {
+		// TODO Auto-generated constructor stub
+		this.base=menuObj;
+		findItem();
+	}
+
+
+	/**
+	 * Find item.
+	 */
+	private void findItem(){	
+		while(hasNext()){
+			//System.out.println("loop");
+			MenuItem aniObj = base.get(position);
+			if(aniObj.isHeartHealthy()==true){
+				//position++;
+				//System.out.println(aniObj+"<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<");
+				break;
+			}else{
+				position++;
+			}
+		}
+		
+	}
+	
+	/* (non-Javadoc)
+	 * @see org.Iterators.MenuIterator#hasNext()
+	 */
+	@Override
+	public boolean hasNext() {
+		//System.out.println("hasNext="+position+"<<<<<<<<<<<<<"+base.size());
+		if (position < base.size()){
+			return true;
+		}
+		else{
+			return false;
+		}
+	}
+
+	/* (non-Javadoc)
+	 * @see org.Iterators.MenuIterator#next()
+	 */
+	@Override
+	public Object next() {	
+		//System.out.println("BEFORE="+position+"<<<<<<<<<<<<<"+base.size());
+		
+		MenuItem aniObj = base.get(position);
+		//System.out.println(aniObj);
+		
+		position++;
+		findItem();
+		//System.out.println("AFTER="+position+"<<<<<<<<<<<<<"+base.size());
+		//System.out.println("============================");
+		return aniObj;
+	}
+
+	/* (non-Javadoc)
+	 * @see org.Iterators.MenuIterator#remove()
+	 */
+	@Override
+	public void remove() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
