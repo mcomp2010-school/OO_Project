1f5b6c1fbbc4ae8886f4f8d0fdcff0332a01225d
Manny <manny01@manny-pc>
2013-04-11 00:08:13 -0400

    Python XMLRPC Client

---
+++ ProjectOO/python/client.py
@@ -0,0 +1,33 @@
+'''
+Created on Apr 10, 2013
+py2.7
+@author: erivera
+'''
+import xmlrpclib
+
+s = xmlrpclib.ServerProxy('http://127.0.0.1:8081/xmlrpc')
+
+commands={}
+commands['get_complete_menu'] = s.SystemInterface.getGetCompleteMenu() # CompleteMenu
+
+
+
+while True:
+    print 'Hello Welcome to the Python XMLRPC client'
+    print '='*40
+    print 'Type in "1" for CompleteMenu()'
+    print '#:',
+    input=raw_input()
+
+    print '=-'*15
+    if '1' == input:
+        print commands['get_complete_menu']
+
+    
+    
+    print '=-'*15
+
+    if 'exit' in input:
+        break
+
+    print '\n'*2
